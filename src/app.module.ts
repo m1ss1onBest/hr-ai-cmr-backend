07f982fcaf43463995259c9493eb2e6f563efb4c
m1ss1onBest <m1poisthbexxxx@gmail.com>
2026-03-25 15:59:46 +0200

    wip/jwt-tokens-service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { DatabaseModule } from './shared/infrastructure/database/database.module
 import { UsersModule } from './api/users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { VacanciesModule } from './api/vacancies/vacancies.module';
+import { AuthModule } from './shared/infrastructure/auth/auth.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { VacanciesModule } from './api/vacancies/vacancies.module';
     DatabaseModule,
     UsersModule,
     VacanciesModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
