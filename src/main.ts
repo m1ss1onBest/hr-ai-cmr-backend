3d74f5714ef6d9d03a99187bf0e3b6b22316bcd8
m1ss1onBest <m1poisthbexxxx@gmail.com>
2026-03-26 21:04:52 +0200

    fix/resolve-conflict

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-<<<<<<< HEAD
-=======
-
   app.enableVersioning({ type: VersioningType.URI });
 
->>>>>>> 7fd024f (wip/candidates-search)
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -19,10 +15,7 @@ async function bootstrap() {
     }),
   );
 
-<<<<<<< HEAD
   await app.listen(process.env.PORT ?? 5000);
-=======
   await app.listen(process.env.PORT ?? 3000);
->>>>>>> 7fd024f (wip/candidates-search)
 }
 void bootstrap();
