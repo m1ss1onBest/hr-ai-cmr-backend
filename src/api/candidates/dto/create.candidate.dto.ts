45c9d9f9eab470cd8110e849329acc057cf5fdd4
m1ss1onBest <m1poisthbexxxx@gmail.com>
2026-03-27 16:27:48 +0200

    feat/kan-52/connect-swagger

--- src/api/candidates/dto/create.candidate.dto.ts
+++ src/api/candidates/dto/create.candidate.dto.ts
@@ -1,24 +1,41 @@
 import { IsNotEmpty, IsString, IsUrl, MaxLength } from 'class-validator';
 import { CandidateBaseResponse } from './candidate.base-response';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCandidateRequest {
   @IsString()
   @IsNotEmpty()
   @MaxLength(255)
+  @ApiProperty({
+    description: 'Candidate real first and last name',
+    example: 'John Doe',
+  })
   name: string;
 
   @IsUrl()
   @MaxLength(255)
+  @ApiProperty({
+    description: 'CV file url',
+    example: 'https://cv.storage.domain/jonh-cv.pdf',
+  })
   cvUrl: string;
 
   @IsString()
   @IsNotEmpty()
   @MaxLength(255)
+  @ApiProperty({
+    description: 'Expected salary value',
+    example: '4600 USD',
+  })
   expectedSalary: string;
 
   @IsString()
   @IsNotEmpty()
   @MaxLength(255)
+  @ApiProperty({
+    description: 'Candidate position',
+    example: 'Senior DevOps Engineer',
+  })
   position: string;
 }
 
